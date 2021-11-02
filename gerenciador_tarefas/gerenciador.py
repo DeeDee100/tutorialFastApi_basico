3b0ec78f21106dc565bc4b07354b3f80d919faa0
DeeDee <amanda_tinoco00@hotmail.com>
2021-11-01 23:52:32 -0300

    Adicionado metodo POST

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,28 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, constr
+from uuid import UUID, uuid4
+from enum import Enum
 
 app = FastAPI()
 
-TAREFAS = [
-		{
-		"id": "1",
-		"titulo": "Familia Addams(2019)",
-		"descrição": "Animação",
-		"estado": "Finalizado",
-	},
-	{
-		"id": "2",
-		"titulo": "V de Vingança (2005)",
-		"descrição": "Na luta pela liberdade, um vigilante, conhecido apenas como V, utiliza-se de táticas terroristas para enfrentar os opressores da sociedade.",
-		"estado": "Finalizado",
-	},
-	{
-		"id": "3",
-		"titulo": "US (2019)",
-		"descrição": "clone assassino",
-		"estado": "não finalizado",
-	},
-]
+class EstadosPossiveis(str, Enum):
+	finalizado = "Finalizado"
+	nao_finalizado = "Nao Finalizado"
+
+class TarefaEntrada(BaseModel):
+	titulo: str
+	descricao: constr(min_length=3)
+	estado: EstadosPossiveis =  EstadosPossiveis.nao_finalizado
+
+class Tarefa(TarefaEntrada):
+	id: UUID
+
+TAREFAS = []
 
 @app.get("/tarefas")
 def listar():
 	return TAREFAS
+
+@app.post('/tarefas', response_model=Tarefa, status_code=201)
+def criar(tarefa: TarefaEntrada):
+	nova_tarefa = tarefa.dict()
+	nova_tarefa.update({"id": uuid4()})
+	TAREFAS.append(nova_tarefa)
+	return nova_tarefa
+
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from fastapi import status
+from starlette.status import HTTP_201_CREATED
 from gerenciador_tarefas.gerenciador import app, TAREFAS
 
 def test_lista_tarefas_retorno_200():
@@ -44,3 +45,74 @@ def test_listar_tarefa_retornada_estado():
 	response = client.get("/tarefas")
 	assert "estado" in response.json().pop()
 	TAREFAS.clear()
+
+def test_aceitar_post():
+	client = TestClient(app)
+	response = client.post("/tarefas")
+	assert response.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
+	TAREFAS.clear()
+
+def test_tarefa_deve_ter_titulo():
+	client = TestClient(app)
+	response = client.post("/tarefas", json={})
+	assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+	TAREFAS.clear()
+
+def test_tarefa_deve_ter_descricao():
+	client = TestClient(app)
+	response = client.post("/tarefas", json={"titulo": "Addams"})
+	assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+	TAREFAS.clear()
+
+def test_tarefa_deve_ter_descricao_min_5_caracteres():
+	client = TestClient(app)
+	response = client.post("/tarefas", json={"titulo": "Addams", "descricao":"A"})
+	assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+	TAREFAS.clear()
+
+# def test_tarefa_deve_ter_estado():
+# 	client = TestClient(app)
+# 	response = client.post("/tarefas", json={"titulo": "Addams", "descricao":"A121223"})
+# 	assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+def test_criar_tarefa_retorna_tarefa():
+	client = TestClient(app)
+	tarefa = {"titulo": "Nancy Drew", "descricao":"A121223"}
+	response = client.post("/tarefas", json=tarefa)
+	nova_response = dict(response.json())
+	nova_response.pop("id")
+	nova_response.pop("estado")
+	assert nova_response == tarefa
+	TAREFAS.clear()
+
+def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
+	cliente = TestClient(app)
+	tarefa1 = {"titulo": "titulo1", "descricao": "descricao1"}
+	tarefa2 = {"titulo": "titulo2", "descricao": "descricao1"}
+	resposta1 = cliente.post("/tarefas", json=tarefa1)
+	resposta2 = cliente.post("/tarefas", json=tarefa2)
+	assert resposta1.json()["id"] != resposta2.json()["id"]
+	TAREFAS.clear()
+
+def test_tarefa_criada_estado_padrao_naoFinalizado():
+	client = TestClient(app)
+	tarefa = {"titulo": "Nancy Drew", "descricao":"A121223"}
+	response = client.post("/tarefas", json=tarefa)
+	assert response.json()["estado"] == "Nao Finalizado"
+	TAREFAS.clear()
+
+def test_POST_codigo_retorno_201():
+	client = TestClient(app)
+	tarefa = {"titulo": "Nancy Drew", "descricao":"descricao"}
+	response = client.post("/tarefas", json=tarefa)
+	assert response.status_code == HTTP_201_CREATED
+	TAREFAS.clear()
+
+def test_salvar_ao_criar():
+	client = TestClient(app)
+	tarefa = {"titulo": "Nancy Drew", "descricao":"descricao"}
+	response = client.post("/tarefas", json=tarefa)
+	assert response.status_code == 201
+	assert len(TAREFAS) == 1
+	TAREFAS.clear()
+
